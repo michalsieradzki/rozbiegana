eb144f79866654990db32d95511dbdcb50205e9a
michalsieradzki <michal.sieradzki@mail.com>
2024-01-02 13:41:06 +0100

    try to deploy1

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,6 +4,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from 'app/assets/stylesheets', under: 'stylesheets'
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
