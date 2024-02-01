1d08eaa48e885f100b0efd77f6b554ce3f759f2f
michalsieradzki <michal.sieradzki@mail.com>
2024-02-01 19:37:08 +0100

    config mailer for reset password first try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,27 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "rozbiegana24_production"
 
-  config.action_mailer.perform_caching = false
+  config.active_storage.service = :local
 
+  # config.action_mailer.delivery_method = :letter_opener
+  
+  MAIL SERVER CONFIGURATION
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.mail.com',
+      port: 587,
+      domain: 'mail.com',
+      user_name: 'michal.sieradzki@mail.com',
+      password: '',
+      authentication: 'plain',
+      enable_starttls_auto: true
+    }
+    
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
