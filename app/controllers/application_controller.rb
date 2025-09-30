033669a6a90ff10ea1c597832978810dff1bd820
michalsieradzki <michal.sieradzki@mail.com>
2025-09-30 09:44:14 +0200

    add authenticate_user! method to applcation controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   # Temporary fix for CSRF issues in production
   skip_forgery_protection if Rails.env.production?
+  
+  # Devise authentication - dodaj dla wszystkich controllerów
+  before_action :authenticate_user!
 end
